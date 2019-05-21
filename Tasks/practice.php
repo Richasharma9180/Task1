0ec472c980f5c63808da2ee5fec6e4cc9d753a11
Richasharma9180 <45814423+Richasharma9180@users.noreply.github.com>
2019-05-21 10:47:54 +0530

    Add files via upload

---
+++ Tasks/practice.php
@@ -0,0 +1,295 @@
+<!DOCTYPE html>
+<html>
+   <head>
+      <title>Welcome</title>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+      <link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.7.0/css/all.css' integrity='sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ' crossorigin='anonymous'>
+   </head>
+   <body style="background-image: url(pic.jpeg);">
+      <div>
+         appy
+      </div>
+      <nav style="text:white;text-align:center;padding:10px;">
+         <span style="color:white; <a href="#">Home</a></span>
+         <span style="color:white;<a href="#">About</a></span>
+         <span style="color:white;<a href="#">Features</a></span>
+         <span style="color:white;<a href="#">Gallery</a></span>
+         <span style="color:white;<a href="#">Pricing</a></span>
+         <span style="color:white;<a href="#">fAQ</a></span>
+         <span style="color:white;<a href="#">Blog</a></span>
+         <span style="color:white;<a href="#">Contacts</a></span>
+      </nav>
+      <div align="right">
+         <a href="#"><button type="submit">Sign Up</button></a>
+      </div>
+      </nav><br><br><br><br><br>
+      <section style="padding:10px;text:white;color:white;">
+         <div>
+            <div align="center">
+               <h3>Start your amazing stuff through appy</h3>
+               <p>
+               <h6>
+               Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+               <h6>
+               </p>
+               <a href ="#"><button style="font-size:25px;color:blue;">Downlaod App <i class="fa fa-download"></i></button></a>
+            </div>
+         </div>
+      </section>
+      <br><br><br><br><br><br><br><br><br><br><br>
+      <header style="background-color:white;padding:20px;text-align:left">
+         <h1>@appy</h1>
+         <nav style="background-color:black;text:white;text-align:center;padding:10px;">
+            <span style="color:white; <a href="#">Home</a></span>
+            <span style="color:white;<a href="#">About</a></span>
+            <span style="color:white;<a href="#">Features</a></span>
+            <span style="color:white;<a href="#">Gallery</a></span>
+            <span style="color:white;<a href="#">Pricing</a></span>
+            <span style="color:white;<a href="#">fAQ</a></span>
+            <span style="color:white;<a href="#">Blog</a></span>
+            <span style="color:white;<a href="#">Contacts</a></span>
+            <div align="right">
+               <a href="#"><button type="submit">Sign Up</button></a>
+            </div>
+         </nav>
+      </header>
+      <br><br><br><br>
+      <section>
+         <div align="center">
+            <h1> About appy </h1>
+         </div>
+         <br><br><br><br><br>
+         <article style="text-align:center;color:blue;padding:20px;font-size:30px;">
+            Bootstrap includes HTML and CSS based design templates for typography, forms, buttons, tables, navigation, modals, image carousels and many other, as well as optional JavaScript plugins
+         </article>
+         <div style="text-align:center">
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+         </div>
+      </section>
+      <br><br><br><br><br><br><br><br><br><br><br>
+      <section>
+         <h3>our progress</h3>
+         <aside style="text-align:left;font:size:35px">
+            <strong>Grat Application</strong>
+            <div style="text-align:left">
+               <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+            </div>
+            <div style>
+               <button type="submit"><span style="color:cyan;font-weight:bold" >Learn More</span></button>
+            </div>
+         </aside>
+      </section>
+      <br><br><br><br><br><br><br><br><br><br>
+      <section>
+        
+         <aside style="text-align:Right;margin:Right">
+         <strong>Grat Application</strong>
+            <div style="text-align:Right">
+               <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+            </div>
+            <div >
+               <button type="submit"><span style="color:cyan;font-weight:bold" >Learn More</span></button>
+            </div>
+         </aside>
+      </section>
+      <br><br><br><br><br><br><br><br><br><br><br>
+      <section>
+         
+         <h3>Powerful feature as always</h3>
+       
+         <div>
+         Fast & Powerful
+      
+         <br><br>
+         Easy Editable
+         <br><br>
+         
+           cloud & storage
+         </div>
+
+         <div align="right">Easy notifications<br><br>
+         Fully Responsive<br><br>
+         Editable Layout<br><br>
+         </div>
+      </section>
+      <br><br><br><br><br><br><br><br><br><br><br>
+      <section>
+         <h1>Our Client Love us</h1>
+         <br><br>
+         <div>
+            <address style="text-align:Left;color:black;padding:20px;font-size:30px;">
+              593 Manor Rd. 
+               Venice, FL 34293
+               35 Sycamore Court 
+               Arlington, MA 02474
+            <address style="text-align:Center;color:black;padding:20px;font-size:30px;">
+               593 Manor Rd. 
+               Venice, FL 34293
+               35 Sycamore Court 
+               Washington, MA 02474
+            </address>
+            <address style="text-align:Right;color:black;padding:20px;font-size:30px;">
+               593 Manor Rd. 
+               Venice, FL 34293
+               35 Sycamore Court 
+               Shicago, MA 02474
+            </address>
+         </div>
+      </section>
+      <br><br><br><br><br><br><br><br><br><br><br>
+      <section>
+         <h1>Screenshots</h1>
+         <br>
+         <aside style="text-align:Right:">
+            <h3>Screeshot 01</h3>
+            <div style="text-align:Right">
+               <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+            </div>
+            <div >
+               <button type="submit"><span style="color:blue;font-weight:bold" >Learn More</span></button>
+            </div>
+         </aside>
+      </section>
+      <br><br><br><br><br><br><br><br><br><br><br>
+      <section>
+      <h3>Our Features</h3>
+      <br><br>
+      <div style="text-align:Right:">
+         <p>Our Approach si simple
+         <p>
+         <div style="text-align:left">
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+               Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+            </p>
+         </div>
+         <div >
+            <button type="submit"><span style="color:blue;font-weight:bold" >Learn More</span></button>
+         </div>
+      </div>
+      <aside style="text-align:right">
+         <br><br>
+         Fast & Powerful
+         <div>
+            <p>it is fast and yest powerful</p>
+         </div>
+         <br><br>
+         Easy Editable
+         <div>
+            <p>it is easy to access</p>
+         </div>
+         <br><br>
+         cloud & storage
+         <div>
+            <p>it is gud for store the info</p>
+            <p></p>
+         </div>
+      </aside>
+      <br><br><br>
+      <section style="text-align:right">
+         <h1>Download App</h1>
+         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+         </p>
+         <div>
+            <button type="submit"><span style="color:blue;font-weight:bold" >Apple Store</span></button>
+            <button type="submit"><span style="color:blue;font-weight:bold" >Play Store</span></button>
+         </div>
+      </section>
+      <br><br><br><br><br><br><br><br><br><br><br>
+      <section style="text-align:center">
+         <h6>Pricing</h6>
+         Our awesome pricing plan
+         <div>
+         <button type="submit"><span style="color:blue;font-weight:bold">Purchase</span></button>
+         <button type="submit"><span style="color:blue;font-weight:bold">Purchase</span></button>
+         <button type="submit"><span style="color:blue;font-weight:bold">Purchase</span></button>
+      </section>
+      <br><br><br><br><br><br><br><br><br><br><br>
+      <section style="text-align:center">
+         <h6>FAQ</h6>
+         Frequently Asked questions
+         <div align="left" >
+            <div>
+               <h3>First</h3>
+               <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                  Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+               </p>
+            </div>
+            <div>
+               <h3>Second </h3>
+               <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                  Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+               </p>
+            </div>
+            <div>
+               <h3>third</h3>
+               <p>
+                  Next questions come
+                  <link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.7.0/css/all.css' integrity='sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ' crossorigin='anonymous'>
+                  here
+               </p>
+            </div>
+         </div>
+         <div align="right">
+         <div>
+            <h3>Fourth</h3>
+            <p>Next questions come here</p>
+         </div>
+         <div>
+            <h3>Fifth</h3>
+            <p> Next questions come here</p>
+         </div>
+         <div>
+         <h3>Sixth</h3>
+         <p> Next questions come here</p>
+         <div></div>
+      </section>
+      <br><br><br><br><br>
+      <section style="text-align:center;color:blue;font-size:35px;padding:10px;">
+         <form>
+            <h1>Subscribe for more features</h1>
+            <br>
+            <input type="text" name="email" placeholder="Enter your mail">
+            <button type="submit">Subscribe</button>
+         </form>
+      </section><br><br><br><br><br><br><br><br><br>
+
+
+      <section style="text-align:center;color:black;font-size:35px">
+         Contact us<br><br>
+         Find our details below
+         <div>
+            <div align="left">
+               <i class='fas fa-phone-volume'>Call us</i>
+               <p>Make  call to us</p>
+            </div>
+            <div align="center">
+               <span class="fa-stack fa-lg">
+               <i class="fas fa-circle fa-stack-2x"></i>
+               <i class="fab fa-twitter fa-stack-1x fa-inverse"></i>
+               </span>
+               fa-twitter <br>
+            </div>
+         </div>
+      </section>
+      <br><br><br><br><br>
+      <footer style="text-align:center;color:black;">
+         <div>
+            <p>Copy right this @2019</p>
+            <p>Contact information: <a href="mailto:someone@example.com">
+               copyright information</a>.
+            </p>
+            <nav style="background-color:white;text:white;text-align:right;padding:10px;">
+               <a href="#">Home</a>
+               <a href="#">About</a>
+               <a href="#">Features</a>
+               <a href="#">Services</a>
+               <a href="#">Pricing</a>
+               <a href="#">Testimonials</a>
+               <a href="#">Contacts</a>
+            </nav>
+         </div>
+      </footer>
+   </body>
+</html>
